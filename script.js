ac326f98ed8481c3e272eb5299da6fdc4708e74f
Ansar Shiekh <134736426+shiekhansar62@users.noreply.github.com>
2024-11-10 23:02:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+"use strict";
+var _a;
+(_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    var _a;
+    event.preventDefault();
+    // Get references to form elements using their IDs
+    const profilePictureInput = document.getElementById('profilePicture');
+    const nameElement = document.getElementById('name');
+    const emailElement = document.getElementById('email');
+    const phoneElement = document.getElementById('phone');
+    const addressElement = document.getElementById('address');
+    const educationElement = document.getElementById('education');
+    const experienceElement = document.getElementById('experience');
+    const skillsElement = document.getElementById('skills');
+    if (profilePictureInput && nameElement && emailElement && phoneElement && addressElement && educationElement && experienceElement && skillsElement) {
+        const name = nameElement.value;
+        const email = emailElement.value;
+        const phone = phoneElement.value;
+        const address = addressElement.value;
+        const education = educationElement.value;
+        const experience = experienceElement.value;
+        const skills = skillsElement.value;
+        // Handle profile picture
+        const profilePictureFile = (_a = profilePictureInput.files) === null || _a === void 0 ? void 0 : _a[0];
+        const profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';
+        // Create resume output
+        const resumeOutput = `
+        <h2>Resume</h2>
+        ${profilePictureURL ? `<img src="${profilePictureURL}" alt="Profile Picture" class="profilePicture">` : ''}
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone Number:</strong> ${phone}</p>
+        <p><strong>Address:</strong> ${address}</p>
+
+        <h3>Education</h3>
+        <p>${education}</p>
+
+        <h3>Work Experience</h3>
+        <p>${experience}</p>
+
+        <h3>Skills</h3>
+        <p>${skills}</p>
+        `;
+        const resumeOutputElement = document.getElementById('resumeOutput');
+        if (resumeOutputElement) {
+            resumeOutputElement.innerHTML = resumeOutput;
+        }
+        else {
+            console.error('The resume output element is missing');
+        }
+    }
+    else {
+        console.error('One or more form elements are missing');
+    }
+});
